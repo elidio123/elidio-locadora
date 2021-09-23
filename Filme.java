1c7da98296fb5ec9a35a3dcbad2b8a1a4a9def72
elidio123 <elidio.laureano@fatec.sp.gov.br>
2021-09-23 18:46:58 +0000

    Projeto final locadora

--- Filme.java
+++ Filme.java
@@ -1,47 +1,60 @@
-public class Filme{
-    private in codigo;
+public class Filme {
+    //nome da classe
+    private int codigo;
+    //descrição tipo int do código
     private String nome;
+    //descrição nome
     private double valor;
+    //descrição double do valor
     private boolean disponivel;
+    //descrição boolean se disponível
     
     public int getCodigo(){
         return codigo;
+        //get faz a consulta
     }
-    public void setCodigo(in codigo){
+    public void setCodigo(int codigo){
         this.codigo = codigo;
+        //set faz a alteração
     }
     public String getNome(){
         return nome;
+        //get faz a consulta
     }
     public void setNome(String nome){
         this.nome = nome;
+        //set faz a alteração
     }
-    public double getValor(){
-        return = valor;
+    public double getValor(){       
+        return  valor;
+        //get faz a consulta
     }
     public void setValor(double valor){
         this.valor = valor;
+        //set faz a alteração
     }
-    public boolean getDisponivel(){
-        return = disponivel;
+    public boolean isDisponivel(){
+        return  disponivel;
+        //descreve o valor 
     }
     public void setDisponivel(boolean disponivel){
-        this.disponevel = disponivel;
+        this.disponivel = disponivel;
+        //set faz a alteração
     }
     public void retirar(){
         if(disponivel == true){
-            disponevel = false;
+            disponivel = false;
         }else{
             throw new RuntimeException("Este filme não esta disponível par locação");
         }
     }
     public void devolver(){
-        disponevel = true;
+        disponivel = true;
     }
-    public void mostra(){
+    public void mostrar(){
         System.out.println("Código do filme:"+ this.codigo);
         System.out.println("Nome do filme:"+ this.nome);
         System.out.println("Valor:R$"+ this.valor);
-        System.out.println("Disponivel:"+ this.disponevel);
+        System.out.println("Disponivel:"+ this.disponivel);
     }
 } 
